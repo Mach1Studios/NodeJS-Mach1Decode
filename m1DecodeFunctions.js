393dab60fdf36b47e8e1d8c0d35ad96e9421cb40
Dylan Marcus <dylan@mach1studios.com>
2021-03-26 11:34:15 -0600

    mach1decode api init

---
+++ m1DecodeFunctions.js
@@ -0,0 +1,7 @@
+const Player = new Mach1SoundPlayer(getAudioFiles(audioFiles8));
+const DecodeModule = new Mach1DecodeModule();
+
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
+
