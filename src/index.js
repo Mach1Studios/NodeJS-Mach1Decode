d14e630d6cc6fbef93563e3e1de11e253941e99d
Anton Tamakhin <snyypi@gmail.com>
2021-06-21 15:52:41 +0300

    bugfix: re-export default services

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as Mach1SoundPlayer } from './services/Mach1SoundPlayer';
+export { default as Mach1Renderer } from './services/Mach1Renderer';
+export { default as Gimbal } from './services/Gimbal';
